b96afc7ba5e53d6650bf8710942e1f2ee5c75dd9
Daniel Bunting <daniel.bunting14@imperial.ic.ac.uk>
2015-06-14 16:31:45 +0100

    - Adds batch mode, allowing automated training over a range of parameters

--- main.cpp
+++ main.cpp
@@ -20,6 +20,94 @@
 #include "evaluate.cpp"
 
 
+
+void run(MCData_t data, TTree* mlp_training, TString* ssp, Int_t * n, TTree* evaluate, Double_t* auc, Double_t* max_sig)
+{
+	
+	TString fname = "plots/" + ssp->ReplaceAll(TString(':'), 1, TString('_'), 1) + "," + TString::Itoa(*n, 10);
+	ssp->ReplaceAll(TString('_'), 1, TString(':'), 1);
+	
+	stringstream *captured_cout = new stringstream() ;	
+	TMultiLayerPerceptron* mlp = TrainNetworkROOT(data.merged, "@MH, @dEta, @dPhi, @Angle, @pBalance, @EtaH, @Sphericity:" + *ssp + ":1:isSignal", captured_cout, fname, *n);
+	evaluate = Evaluate(mlp, data, 100);
+	
+	*max_sig = SignificancePlot(evaluate, fname);		
+	ErrorPlot(captured_cout, fname);
+	*auc = ROCPlot(evaluate, fname);
+	SeparationPlot(data, mlp, fname);
+	
+	mlp_training->Fill();
+}
+
+
+void interactive(MCData_t data, TTree* mlp_training, TString* ssp, Int_t * n, TTree* evaluate, Double_t* auc, Double_t* max_sig )
+{
+	
+	TString n_epoch;	
+	while(1)
+	{	
+		cout << "Please enter a MLP structure string or q to save and quit:" << endl;
+		cin >> *ssp; 
+		if (*ssp == "q") { break;}
+		
+		cout << "Please enter a number of epochs to train for or q to save and quit:" << endl;
+		cin >> n_epoch;
+		if (n_epoch == "q") { break;}
+		
+		*n = n_epoch.Atoi();		
+		
+		run(data, mlp_training , ssp, n, evaluate, auc, max_sig);
+	
+	}
+}
+
+
+
+
+void batch(MCData_t data, TTree* mlp_training, TString* ssp, Int_t * n, TTree* evaluate, Double_t* auc, Double_t* max_sig )
+{
+	
+	TString n_epoch, batch_ss;
+	Int_t min, max, step;	
+	
+	cout << "Please enter a MLP structure string, substituting x for the layer to iterate over or q to save and quit:" << endl;
+	cin >> batch_ss; 
+	if (batch_ss == "q") { return;}
+	
+	cout << "Please enter a start value for x:" << endl;
+	cin >> min; 
+	cout << "Please enter a stop value for x:" << endl;
+	cin >> max;  
+	cout << "Please enter a step size for x:" << endl;
+	cin >> step; 
+	
+	cout << "Please enter a number of epochs to train for or q to save and quit:" << endl;
+	cin >> n_epoch;
+	if (n_epoch == "q") { return;}
+	
+	*n = n_epoch.Atoi();		
+	
+	
+	cout << "Running:" <<endl;
+	for(Int_t i = min; i <= max; i+=step)
+	{
+		*ssp = batch_ss;
+		ssp->ReplaceAll(TString('x'), TString::Itoa(i, 10));
+		
+		cout << "\tSSP = " << *ssp <<endl;
+	
+		TString fname = "plots/" + ssp->ReplaceAll(TString(':'), 1, TString('_'), 1) + "," + TString::Itoa(*n, 10);
+		ssp->ReplaceAll(TString('_'), 1, TString(':'), 1);
+	
+		run(data, mlp_training , ssp, n, evaluate, auc, max_sig);
+	}
+	
+
+
+}
+
+
+
 int main (int argc, char  *argv[])
 {
 	
@@ -27,13 +115,12 @@ int main (int argc, char  *argv[])
 	
 	//	CreateTraining();
 	
-	MCData_t data = LoadTraining("combined_output.root");
-	
+	MCData_t data = LoadTraining("combined_output.root");	
 	TTree* evaluate =  new TTree();
 	Double_t auc, max_sig = 0.;
 	Int_t n=0;
-	TString ss = "";
-	TString* ssp = &ss;
+
+	TString* ssp = new TString("");
 	
 	/* Open or create a root file to store all the results */
 	
@@ -61,30 +148,20 @@ int main (int argc, char  *argv[])
 		mlp_training->Branch("struct_string", &ssp);
 	}
 	
-	
-	TString n_epoch;	
-	while(1)
-	{	
-		cout << "Please enter a MLP structure string and a number of epochs to train for or q to save and quit:" << endl;
-		cin >> ss >> n_epoch;
-		if (ss == "q") { break;}
-		n = n_epoch.Atoi();
-		TString fname = "plots/" + ss.ReplaceAll(TString(':'), 1, TString('_'), 1) + "," + n_epoch;
-		ss.ReplaceAll(TString('_'), 1, TString(':'), 1);
-		stringstream *captured_cout = new stringstream() ;	
-		TMultiLayerPerceptron* mlp = TrainNetworkROOT(data.merged, "@MH, @dEta, @dPhi, @Angle, @pBalance, @EtaH, @Sphericity:" + ss + ":1:isSignal", captured_cout, fname, n);
-		evaluate = Evaluate(mlp, data, 100);
+	TString mode;
+	cout << "Please enter i for interactive or b for batch mode" <<endl;
+	cin >> mode;
+
+		if (mode == 'i')
+			interactive(data, mlp_training , ssp, &n, evaluate, &auc, &max_sig);
 		
-		max_sig = SignificancePlot(evaluate, fname);		
-		ErrorPlot(captured_cout, fname);
-		auc = ROCPlot(evaluate, fname);
-		SeparationPlot(data, mlp, fname);
+		else if (mode == 'b')
+			batch(data, mlp_training , ssp, &n, evaluate, &auc, &max_sig);
 		
-		mlp_training->Fill();
-		f->Write();
+		else
+			cout << "Please enter i for interactive or b for batch mode" <<endl;
+ 
 	
-	}
- 	
 	f->Write();
 	f->Close();
 	cout << "Done" <<endl;
