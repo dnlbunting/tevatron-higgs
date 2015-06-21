393388f3b7dfea02396f4a87f6ee2373acac28ed
Daniel Bunting <daniel.bunting14@imperial.ic.ac.uk>
2015-06-21 18:35:46 +0100

    - Modifies the way sig is calculated to match the demo

--- evaluate.cpp
+++ evaluate.cpp
@@ -1,8 +1,55 @@
+#include <TStyle.h>
 
-TTree *Evaluate(TMultiLayerPerceptron * mlp, MCData_t data, Double_t nPoints = 100)
+void SeparationPlot(TH1D* sig_hist, TH1D* bkg_hist , TH1D* usig_hist, TH1D* ubkg_hist, TString fname )
 {
+	TStyle* sty = new TStyle();
+	sty->SetOptStat(0);
 	
+	TCanvas *sep_cnavas = new TCanvas("sep_cnavas");		
+	
+	sig_hist->SetFillStyle(3005);
+	sig_hist->SetFillColor(kRed);
+	sig_hist->SetLineColor(kRed); 
+	sig_hist->Draw();
+	sep_cnavas->Update();
+	sep_cnavas->SaveAs(fname + "_signal.pdf");
+	
+	sep_cnavas->Clear();
+	
+	bkg_hist->SetFillStyle(3005);
+	bkg_hist->SetFillColor(kBlue);
+	bkg_hist->SetLineColor(kBlue);
+	bkg_hist->Draw();
+	
+	sep_cnavas->Update();
+	sep_cnavas->SaveAs(fname + "_background.pdf"); 
+
+	usig_hist->SetFillStyle(3005);
+	usig_hist->SetFillColor(kRed);
+	usig_hist->SetLineColor(kRed); 
+	usig_hist->Draw();
+	ubkg_hist->SetFillStyle(3005);
+	ubkg_hist->SetFillColor(kBlue);
+	ubkg_hist->SetLineColor(kBlue);
+	ubkg_hist->Draw("same");
+	usig_hist->GetXaxis()->SetTitle("NN output");
+	sep_cnavas->Update();
+	sep_cnavas->SaveAs(fname + "_seperation_plot.pdf"); 
+
+
+
+}
+
+
+TTree *Evaluate(TMultiLayerPerceptron * mlp, MCData_t data, TString fname, Double_t nPoints = 100)
+{
+	Int_t nbins = 60;
 	Double_t s, b, S, tpr, fpr, cutoff = 0.;
+	
+	TH1D* sig_hist = new TH1D("sig_hist", "Signal", nbins, 0., 1.);
+	TH1D* bkg_hist = new TH1D("bkg_hist", "Background", nbins, 0., 1.);
+	TH1D* ubkg_hist = new TH1D("ubkg_hist", "Un-weighted Background", nbins, 0., 1.);
+	TH1D* usig_hist = new TH1D("usig_hist", "Un-weighted Signal", nbins, 0., 1.);	
 	
 	TTree* evaluate = new TTree("evaluate", "evaluate");
 	evaluate->Branch("cutoff",   &cutoff,   "cutoff/D");	
@@ -10,54 +57,79 @@ TTree *Evaluate(TMultiLayerPerceptron * mlp, MCData_t data, Double_t nPoints = 1
 	evaluate->Branch("fpr",   &fpr,   "fpr/D");	
 	evaluate->Branch("s",   &s,   "s/D");	
 	evaluate->Branch("b",   &b,   "b/D");	
-	evaluate->Branch("S",   &S,   "S/D");	
+	evaluate->Branch("S",   &S,   "S/D");
+
 	
 	Double_t parameters[7], weight;
 	Bool_t isSignal, isTrain;	
 	
-	data.test_signal->SetBranchAddress("MH",   &parameters[0]);	 	        data.test_background->SetBranchAddress("MH",   &parameters[0]);	 	       
-	data.test_signal->SetBranchAddress("dEta", &parameters[1]);	            data.test_background->SetBranchAddress("dEta", &parameters[1]);	           
-	data.test_signal->SetBranchAddress("dPhi",  &parameters[2]);	        	data.test_background->SetBranchAddress("dPhi",  &parameters[2]);	              
-	data.test_signal->SetBranchAddress("Angle",    &parameters[3]);         	data.test_background->SetBranchAddress("Angle",    &parameters[3]);        
-	data.test_signal->SetBranchAddress("pBalance",  &parameters[4]);        	data.test_background->SetBranchAddress("pBalance",  &parameters[4]);    
-	data.test_signal->SetBranchAddress("EtaH", &parameters[5]);	            data.test_background->SetBranchAddress("EtaH", &parameters[5]);	           
-	data.test_signal->SetBranchAddress("Sphericity",  &parameters[6]);	    	data.test_background->SetBranchAddress("Sphericity",  &parameters[6]);	
+	data.test_signal->SetBranchAddress("MH",   &parameters[6]);	 	        data.test_background->SetBranchAddress("MH",   &parameters[6]);	 	       
+	data.test_signal->SetBranchAddress("dEta", &parameters[0]);	            data.test_background->SetBranchAddress("dEta", &parameters[0]);	           
+	data.test_signal->SetBranchAddress("dPhi",  &parameters[1]);	        	data.test_background->SetBranchAddress("dPhi",  &parameters[1]);	              
+	data.test_signal->SetBranchAddress("Angle",    &parameters[2]);         	data.test_background->SetBranchAddress("Angle",    &parameters[2]);        
+	data.test_signal->SetBranchAddress("pBalance",  &parameters[3]);        	data.test_background->SetBranchAddress("pBalance",  &parameters[3]);    
+	data.test_signal->SetBranchAddress("EtaH", &parameters[4]);	            data.test_background->SetBranchAddress("EtaH", &parameters[4]);	           
+	data.test_signal->SetBranchAddress("Sphericity",  &parameters[5]);	    	data.test_background->SetBranchAddress("Sphericity",  &parameters[5]);	
 	data.test_signal->SetBranchAddress("isSignal",   &isSignal);            	data.test_background->SetBranchAddress("isSignal",   &isSignal);   
 	data.test_signal->SetBranchAddress("isTrain",   &isTrain);              	data.test_background->SetBranchAddress("isTrain",   &isTrain);  
 	data.test_signal->SetBranchAddress("weight",   &weight);              		data.test_background->SetBranchAddress("weight",   &weight); 
 	
-	Int_t tp, fp = 0;
-	
-	for(Int_t i = 0; i < nPoints; ++i)
+
+	for(Int_t j = 0; j < data.test_signal->GetEntries(); ++j)
 	{
-		cutoff =  i*(1.0/nPoints);
-		s = 0.;  b = 0.;
-		fp = 0.; tp = 0.;
-		S = 0.;
+		data.test_signal->GetEntry(j);
+		Double_t p = mlp->Evaluate(0,parameters);
+		sig_hist->Fill(p, weight);
+		usig_hist->Fill(p);
+	}
+	
+	for(Int_t j = 0; j < data.test_background->GetEntries(); ++j)
+	{
+		data.test_background->GetEntry(j);
+		Double_t p = mlp->Evaluate(0,parameters);
+		bkg_hist->Fill(p, weight);
+		ubkg_hist->Fill(p);
+	}
+	
+	SeparationPlot(sig_hist, bkg_hist ,usig_hist, ubkg_hist, fname );
+	
+	TH1F *sigacc = new TH1F("sigacc", "sigacc", 60,  0, 1);
+	TH1F *backacc = new TH1F("backacc", "backacc", 60,  0, 1);
+	
+	Int_t tp = 0;
+	Int_t fp = 0;
+	s = 0; b = 0; S = 0;
+	for(Int_t i = 0; i < nbins; ++i)
+	{
+		tp += usig_hist->GetBinContent(nbins-i);
+		fp += ubkg_hist->GetBinContent(nbins-i);
 		
-		for(Int_t j = 0; j < data.test_signal->GetEntries(); ++j)
-		{
-			data.test_signal->GetEntry(j);
-			Double_t p = mlp->Evaluate(0,parameters);
-			if(p > cutoff) {s +=  weight; tp++;}
-		}
+	    s += 100*sig_hist->GetBinContent(nbins-i);
+	    b += bkg_hist->GetBinContent(nbins-i);
+
+	 	S = (b > 0) ? s / sqrt(b+(0.02*0.02*b*b)) : 0.;		
+		cutoff = 1 - i/(float)nbins;
 		
-		for(Int_t j = 0; j < data.test_background->GetEntries(); ++j)
-		{
-			data.test_background->GetEntry(j);
-			Double_t p = mlp->Evaluate(0,parameters);
-			if(p >= cutoff) {b += weight; fp++;}
-		}
+	 //   cout << S << "\t" << s<< "\t" <<b<<"\t"<< cutoff << endl; 	
+		sigacc->SetBinContent(nbins-i, s);
+		backacc->SetBinContent(nbins-i, b);
 		
 		tpr = tp /(Double_t) data.test_signal->GetEntries();
 		fpr = fp /(Double_t) data.test_background->GetEntries();
-		
-		S = (b > 0) ? s / sqrt(b+(0.02*0.02*b*b)) : 0.;
+		//cout << tp << "\t" <<fp <<endl;		
 		evaluate->Fill();
-		
 	}
-
+	
+	TCanvas *acc_canvas=new TCanvas("acc_canvas");
+	sigacc->SetLineColor(kRed);
+	backacc->Draw();
+	acc_canvas->SaveAs(fname + "_backacc.pdf"); 	
+	sigacc->Draw();
+	acc_canvas->SaveAs(fname + "_sigacc.pdf"); 
+					
+			
 	return(evaluate);
+
 }
 
 
@@ -115,78 +187,6 @@ Double_t SignificancePlot(TTree* evaluate, TString fname)
 }
 
 
-void SeparationPlot(MCData_t data, TMultiLayerPerceptron *mlp, TString fname, Int_t nbins = 60)
-{
-	
-	TH1D* sig_hist = new TH1D("sig_hist", "Signal", nbins, 0., 1.);
-	TH1D* bkg_hist = new TH1D("bkg_hist", "Background", nbins, 0., 1.);
-	TH1D* ubkg_hist = new TH1D("ubkg_hist", "Un-weighted Background", nbins, 0., 1.);
-	TH1D* usig_hist = new TH1D("usig_hist", "Un-weighted Signal", nbins, 0., 1.);	
-	
-	Double_t parameters[7], weight;
-	Bool_t isSignal, isTrain;	
-	
-	data.test_signal->SetBranchAddress("MH",   &parameters[0]);	 	        	data.test_background->SetBranchAddress("MH",   &parameters[0]);	 	       
-	data.test_signal->SetBranchAddress("dEta", &parameters[1]);	            	data.test_background->SetBranchAddress("dEta", &parameters[1]);	           
-	data.test_signal->SetBranchAddress("dPhi",  &parameters[2]);	        	data.test_background->SetBranchAddress("dPhi",  &parameters[2]);	              
-	data.test_signal->SetBranchAddress("Angle",    &parameters[3]);         	data.test_background->SetBranchAddress("Angle",    &parameters[3]);        
-	data.test_signal->SetBranchAddress("pBalance",  &parameters[4]);        	data.test_background->SetBranchAddress("pBalance",  &parameters[4]);    
-	data.test_signal->SetBranchAddress("EtaH", &parameters[5]);	            	data.test_background->SetBranchAddress("EtaH", &parameters[5]);	           
-	data.test_signal->SetBranchAddress("Sphericity",  &parameters[6]);	    	data.test_background->SetBranchAddress("Sphericity",  &parameters[6]);	
-	data.test_signal->SetBranchAddress("isSignal",   &isSignal);            	data.test_background->SetBranchAddress("isSignal",   &isSignal);   
-	data.test_signal->SetBranchAddress("isTrain",   &isTrain);              	data.test_background->SetBranchAddress("isTrain",   &isTrain);  
-	data.test_signal->SetBranchAddress("weight",   &weight);              		data.test_background->SetBranchAddress("weight",   &weight); 
-	
-	for(Int_t j = 0; j < data.test_signal->GetEntries(); ++j)
-	{
-		data.test_signal->GetEntry(j);
-		Double_t p = mlp->Evaluate(0,parameters);
-		sig_hist->Fill(p, weight);
-		usig_hist->Fill(p);
-	}
-	
-	for(Int_t j = 0; j < data.test_background->GetEntries(); ++j)
-	{
-		data.test_background->GetEntry(j);
-		Double_t p = mlp->Evaluate(0,parameters);
-		bkg_hist->Fill(p, weight);
-		ubkg_hist->Fill(p);
-	}
-	
-	TCanvas *sep_cnavas = new TCanvas("sep_cnavas");		
-	
-	sig_hist->SetFillStyle(3005);
-	sig_hist->SetFillColor(kRed);
-	sig_hist->SetLineColor(kRed); 
-	sig_hist->Draw();
-	sep_cnavas->Update();
-	sep_cnavas->SaveAs(fname + "_signal.pdf");
-	
-	sep_cnavas->Clear();
-	
-	bkg_hist->SetFillStyle(3005);
-	bkg_hist->SetFillColor(kBlue);
-	bkg_hist->SetLineColor(kBlue);
-	bkg_hist->Draw();
-	
-	sep_cnavas->Update();
-	sep_cnavas->SaveAs(fname + "_background.pdf"); 
-
-	usig_hist->SetFillStyle(3005);
-	usig_hist->SetFillColor(kRed);
-	usig_hist->SetLineColor(kRed); 
-	usig_hist->Draw();
-	ubkg_hist->SetFillStyle(3005);
-	ubkg_hist->SetFillColor(kBlue);
-	ubkg_hist->SetLineColor(kBlue);
-	ubkg_hist->Draw("same");
-	
-	sep_cnavas->Update();
-	sep_cnavas->SaveAs(fname + "_seperation_plot.pdf"); 
-
-
-
-}
 
 
 void ErrorPlot(stringstream *ss, TString fname)
